712a88bd7c34522bf57c221961a97c699c2a903c
snowjet <rarm.jet@gmail.com>
2019-07-12 21:55:16 +1000

    bind all addresses and port 8080

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app='main:app', host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8080, log_level="info", reload=True)
