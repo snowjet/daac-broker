b5aa0d257e16bd4e69b0b43de21139a71c6e6b08
Rarm Jet <rarm.jet@gmail.com>
2019-07-22 20:57:49 +1000

    ensure DB is connected

--- api/main.py
+++ api/main.py
@@ -77,3 +77,12 @@ def add_user(username: str, dcaas_params: DCaaS_Params):
     logger.info("Attempted to create user", user=username, dc=dc_msg, svc=svc_msg)
 
     return {"user-added": username}
+
+# Admin Functions
+@app.put("/admin/prepare-db")
+def prepare_db():
+
+    guacdb.confirm_db_connection()
+    msg = guacdb.load_schmea_safe()
+
+    return {"prepare-db": msg}
--- guaclibs/db.py
+++ guaclibs/db.py
@@ -61,8 +61,11 @@ class GuacDatabaseAccess:
 
     def confirm_db_connection(self):
 
-        logger.debug("checking if db is connected")
-        if not self.db_conn.get_transaction_status():
+        # Read-only integer attribute: 0 if the connection is open, nonzero if it is closed or broken.
+        conn_status = self.db_conn.closed
+        logger.debug("DB connection status", connection_status=conn_status)
+
+        if conn_status:
             logger.info("DB is not connected - reconnecting")
             self._connect_to_db()
 
@@ -344,11 +347,13 @@ class GuacDatabaseAccess:
         table_exists = cursor.fetchall()
 
         if not table_exists[0][0]:
-            logger.info("Schema Not Found")
+            return_msg = "Schema Not Found - Adding schema"
+            logger.info(return_msg)
             db_schema = os.path.join(os.path.dirname(__file__), "db_schema/initdb.sql")
             sql_file = open(db_schema, "r")
             cursor.execute(sql_file.read())
         else:
-            logger.info("tables already has a schema")
+            return_msg = "tables already has a schema"
+            logger.info(return_msg)
 
-        return True
+        return return_msg
