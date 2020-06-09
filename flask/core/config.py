fd5eff5d4537cd0d79079e281484ca6920ae73f7
Rarm Jet <rarm.jet@gmail.com>
2020-06-09 13:25:10 +1000

    update postgres host env

--- flask/core/config.py
+++ flask/core/config.py
@@ -21,7 +21,7 @@ GUACADMIN_PASSWORD = os.getenv("GUACADMIN_PASSWORD", "guacadmin")
 GUAC_URL = os.getenv("GUAC_URL", "https://guac.apps-crc.testing")
 
 if not DATABASE_URL:
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
+    POSTGRES_HOST = os.getenv("POSTGRES_SERVICE_HOST", "postgres")
     POSTGRES_PORT = int(os.getenv("POSTGRES_SERVICE_PORT", 5432))
     POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASS = os.getenv("POSTGRES_PASSWORD", "postgres")
